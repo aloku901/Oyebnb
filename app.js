b263bf029bf495466eceae6e56a73772c7d6e4cd
Alok Upadhyay <aloku901@gmail.com>
2023-11-25 19:35:37 +0530

    Add frontend part

--- app.js
+++ app.js
@@ -2,37 +2,90 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
+const path = require("path");
+const methodOverride = require("method-override"); 
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/oyebnb";
 
-main().then(() => {
+main()
+  .then(() => {
     console.log("connected to DB");
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method")); 
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.get("/", (req, res) => {
-    res.send("Hii I am ALok");
+  res.send("Hii I am ALok");
 });
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "My new Villa",
-        description: "buy with all city",
-        price: 5000,
-        location: "Azamgarh",
-        country: "India",
-    });
+app.get("/listings", async (req, res) => {
+  const allListing = await Listing.find({});
+  res.render("./listings/index.ejs", {allListing});
+});
 
-    await sampleListing.save();
-    console.log("Sample listing Saved");
-    res.send("Successful Testing Done");
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");  
 })
 
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing})
+})
+
+app.post("/listings", async(req, res) => {
+    let listing = req.body.listing;
+    const newListing = new Listing(listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
+
+app.get("/listings/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+})
+
+app.put("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+app.delete("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    let deleteListing = await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
+
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My new Villa",
+//         description: "buy with all city",
+//         price: 5000,
+//         location: "Azamgarh",
+//         country: "India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("Sample listing Saved");
+//     res.send("Successful Testing Done");
+// })
+
 app.listen(3000, () => {
-    console.log("Server is starting on port 3000");
-});  
+  console.log("Server is starting on port 3000");
+});
